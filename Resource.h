50b10868c4e036118e77334a23a95b8d4183f21d
len4059 <len4059@gmail.com>
2024-12-05 05:31:45 +0800

    test

--- Resource.h
+++ Resource.h
@@ -3,7 +3,7 @@
 
 namespace Resource {
     constexpr char game_icon[] = "assets/image/game_icon.png";
-    constexpr char background_img_path[] = "assets/image/StartBackground.jpg";
+    constexpr char background_img_path[] = "assets/image/earth_background.jpg";
     constexpr char game_start_sound_path[] = "assets/sound/growl.wav";
     constexpr char background_music[] = "assets/sound/BackgroundMusic.ogg";
 
